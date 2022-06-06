6e38851adb4ed2dabdfaaaf842ce4c3ebb8ea2e2
Roman Shalman <romanshalman@gmail.com>
2022-06-06 17:59:22 +0100

    WIP: add assets + fonts + variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { MainPage } from "@presentation/complex/pages/MainPage";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div>
       <main></main>
+      <MainPage />
       <footer></footer>
     </div>
   );
--- presentation/complex/pages/MainPage/index.tsx
+++ presentation/complex/pages/MainPage/index.tsx
@@ -1,6 +1,20 @@
 import { FC } from "react";
+import Image from "next/image";
+import S from "./index.module.scss";
 import { MainPageProps } from "./types";
 
 export const MainPage: FC<MainPageProps> = () => {
-  return <div></div>;
+  return (
+    <div className={S.MainPage}>
+      <div className={S.avatar}>
+        <Image
+          src={"/images/roman-shalman-photo.png"}
+          layout={"fill"}
+          objectFit={"contain"}
+          objectPosition={"bottom"}
+        />
+        <p>Some test text</p>
+      </div>
+    </div>
+  );
 };
