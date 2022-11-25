19e3f7580e8d9cbf19094fefea45bd9518cc3aa3
RShalman <romanshalman@gmail.com>
2022-11-25 08:24:56 +0000

    Add: todo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+//TODO: fix issues from Lighthouse
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
