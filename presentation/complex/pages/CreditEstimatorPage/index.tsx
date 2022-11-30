5737097fbd7c46b2805f3a3268a0ff1a4366dd8c
RShalman <romanshalman@gmail.com>
2022-11-30 12:26:27 +0000

    Upd: credit-estimator as web component

--- presentation/complex/pages/CreditEstimatorPage/index.tsx
+++ presentation/complex/pages/CreditEstimatorPage/index.tsx
@@ -1,18 +1,7 @@
-import CreditEstimator from 'credit-estimator';
-import { useEffect, useRef } from 'react';
-
-const NODE_ID = 'credit-estimator-project';
+import 'credit-estimator';
 
 const CreditEstimatorPage = () => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!ref?.current?.hasChildNodes()) {
-      CreditEstimator(NODE_ID);
-    }
-  }, [ref]);
-
-  return <div ref={ref} id={NODE_ID} />;
+  return <credit-estimator />;
 };
 
 export default CreditEstimatorPage;
--- types/custom.d.ts
+++ types/custom.d.ts
@@ -1,1 +1,13 @@
 export type IFunction = (...args) => unknown;
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      'credit-estimator': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
